27b9455032ec8f69b405d2614541e65e9c664947
kanmi <kanmi@kanmi.org>
2018-06-24 00:49:46 +0900

    Apply eslint and prettier

--- src/index.js
+++ src/index.js
@@ -9,28 +9,29 @@ const token = process.env.SLACK_TOKEN;
 const web = new WebClient(token);
 const rtm = new RTMClient(token);
 
-rtm.on('message', (message) => {
+rtm.on('message', message => {
   // Skip messages that are from a bot or my own user ID or not mentioned to me.
-  if ( (message.subtype && message.subtype === 'bot_message') ||
-       (!message.subtype && message.user === rtm.activeUserId) ||
-       (!message.text.includes(`<@${rtm.activeUserId}>`)) ) {
+  if (
+    (message.subtype && message.subtype === 'bot_message') ||
+    (!message.subtype && message.user === rtm.activeUserId) ||
+    !message.text.includes(`<@${rtm.activeUserId}>`)
+  ) {
     return;
   }
 
   const userId = message.user;
 
-  web.users.info({user: userId})
-    .then((res) => {
-      const user = res.user;
-      rtm.sendMessage(`みくだにゃ`, message.channel)
-        .then((res) => {
+  web.users
+    .info({ user: userId })
+    .then(res => {
+      rtm
+        .sendMessage(`みくだにゃ`, message.channel)
+        .then(res => {
           console.log('Message sent: ', res.ts);
         })
         .catch(console.error);
     })
     .catch(console.error);
-
 });
-
 
 rtm.start();
